042cba121c1afedf249145d31c64b418dd766860
Alleexxi <66967403+Alleexxi@users.noreply.github.com>
2025-09-06 00:17:05 +0200

    asd

--- MachoHookNatives.lua
+++ MachoHookNatives.lua
@@ -1,8 +1,6 @@
 local natives_json = MachoWebRequest("https://raw.githubusercontent.com/Alleexxi/MachoScripts/refs/heads/main/natives_simplified.json")
 local natives = json.decode(natives_json)
 
-print("Hooking natives")
-
 for i,v in ipairs(natives) do
 
     local native_name = type(v.name) == "table" and v.hash or v.name
@@ -10,7 +8,7 @@ for i,v in ipairs(natives) do
         MachoHookNative(%s, function(...)
     local resource = GetCurrentResourceName();
     if resource == nil then return true end
-    --if type(resource) == "string" and resource ~= "WaveShield" then return true end
+    if type(resource) == "string" and resource ~= "ReaperV4" then return true end
     local args = {...}
 
     print(resource .. " called native " .. "%s" .. " with args: " .. json.encode(args))
@@ -19,3 +17,5 @@ for i,v in ipairs(natives) do
   end)
     ]]):format(v.hash, native_name, native_name))()
 end
+
+
